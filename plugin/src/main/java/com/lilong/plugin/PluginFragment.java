5db5bfab160d231527470a0e47a52782c4b8cbf3
lilong <lilong@zhangyue.com>
2018-05-18 11:24:53 +0800

    more

--- plugin/src/main/java/com/lilong/plugin/PluginFragment.java
+++ plugin/src/main/java/com/lilong/plugin/PluginFragment.java
@@ -1,11 +1,24 @@
 package com.lilong.plugin;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
- * Created by lilong on 18-5-17.
+ * 插件fragment
  */
 
 public class PluginFragment extends Fragment{
 
+    private View fragmentRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        fragmentRootView = inflater.inflate(R.layout.plugin_fragment, container, false);
+        return fragmentRootView;
+    }
 }
